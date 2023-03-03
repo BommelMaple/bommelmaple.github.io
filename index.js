636215daaf9a13799d28d3c072c4d55fcd83ef61
BommelMaple <124814997+BommelMaple@users.noreply.github.com>
2023-03-03 14:35:28 +0100

    First working version
    I hope no bugs :)

--- index.js
+++ index.js
@@ -4,23 +4,31 @@ function optimize(option_list){
     // Each object contains {att: xxx%, boss: xxx%, dmg: xxx%, cdmg: xxx%, ied: xxx%}
     // It doesn't return a new list but adds the scores to the initial list
 
-    // Initialize variables
-    var att = null; var boss = null; var dmg = null; var cdmg = null; var ied = null;
-    var fd_score = null;
-    var boss_pdr = 380/100;
-
     // Loop over array
-    $.each(option_list, function(index, object){
-        att  = object.att;
-        boss = object.boss;
-        dmg  = object.dmg;
-        cdmg = object.cdmg;
-        ied  = object.ied;
+    //$.each(option_list, function(index, object){
+    //    object = calculate_fd_score(object);
+    //})
 
+    let sorted_options = option_list.sort((op1, op2) => (op1.fd_score > op2.fd_score) ? -1 : (op1.fd_score < op2.fd_score) ? 1 : 0);
 
-        fd_score = att * (boss + dmg) * cdmg * (1 - (boss_pdr * (1 - ied)));
-        object.fd_score = fd_score;
-    })
+    return sorted_options
+}
+
+function calculate_fd_score(object){
+    // Calculates the FD score for each option
+    var att = null; var boss = null; var dmg = null; var cdmg = null; var ied = null;
+    var boss_pdr = 380/100;
+    var fd_score = null;
+
+    att  = object.att;
+    boss = object.boss;
+    dmg  = object.dmg;
+    cdmg = object.cdmg;
+    ied  = object.ied;
+    fd_score = (att/100) * ((boss + dmg)/100) * (cdmg/100) * (1 - (boss_pdr * (1 - ied)));
+    object.fd_score = fd_score;
+
+    return object;
 }
 
 function add_base_stats_yourself(base_stats_to_add, option_list){
@@ -91,8 +99,9 @@ function add_familiars_options(familiars_list, option_list){
     console.log("FAMILIAR ARRAYS: ", familiars_to_add);
 
     $.each(option_list, function(option_index, option){
+        var JSON_option = JSON.stringify(option);
         $.each(familiars_to_add, function(indx1, familiar_array){
-            new_option = option;
+            new_option = JSON.parse(JSON_option);
             $.each(familiar_array, function(indx2, familiar){
                 $.each(familiar, function(indx3, fam_stats){
                     $.each(fam_stats, function(index, value){
@@ -224,17 +233,52 @@ function combine_all_line_options(prime_line_options, normal_line_options){
 
 function add_hyper_stat_options(available_pts, option_list){
     // This function first gets all possible (maxed) hyper stat combinations
-    // Options assume every stat gets to level 7 first, assumed is that this investment is alway optimal
-    // If you can't get every stat to level 7 initially then wtf are you optimizing
 
     var base_hyper_stats = {"boss":0,"dmg":0,"cdmg":0,"ied":0};
+    var best_fd_score = 0; var lowest_fd_score = 0;
+    var amount_of_scores = 0;
+    var max_amount_of_scores = 1000;
     
 
     // Then create all possible options
     possible_hyper_stat_levels = distributePoints(available_pts);
     possible_hyper_stats = translate_hyper_stat_levels_to_stats(possible_hyper_stat_levels);
 
-    
+    var perct = 0;
+    var new_option_list = [];
+    // Then add all possible hyper stats to each option in the option list
+    $.each(option_list, function(option_index, option){
+        var JSON_option = JSON.stringify(option);
+        $.each(possible_hyper_stats, function(hyper_index, hypers){
+            new_option = JSON.parse(JSON_option);
+            new_option["hypers"] = hypers;
+            new_option["cdmg"] += hypers["cdmg"];
+            new_option["boss"] += hypers["boss"];
+            new_option["dmg"] += hypers["dmg"];
+            new_option["ied"] = calculate_new_ied(new_option["ied"],hypers["ied"]);
+            new_option = calculate_fd_score(new_option);
+
+            // Check if we should add it to the list
+            if( amount_of_scores < max_amount_of_scores ){
+
+                new_option_list.push(new_option);
+                new_option_list = optimize(new_option_list);
+                amount_of_scores++;
+                lowest_fd_score = new_option_list[new_option_list.length - 1].fd_score;
+
+            } else if(new_option.fd_score > lowest_fd_score){
+
+                new_option_list[new_option_list.length - 1] = new_option;
+                new_option_list = optimize(new_option_list);
+                lowest_fd_score = new_option_list[new_option_list.length - 1].fd_score;
+
+            }            
+        })
+        perct = 100*(option_index / option_list.length).toFixed(3);
+        console.log(perct+"%");
+    })
+
+    return new_option_list;
 
 }
 
@@ -258,9 +302,9 @@ function distributePoints(budget) {
             var cost = cumulative_level_up_cost[i];
             if (cost <= budget) {
                 budget -= cost;
-                resource_levels[Index] += i;
+                resource_levels[index] += i;
                 distributeHelper(budget, index + 1);
-                resource_levels[Index] -= i;
+                resource_levels[index] -= i;
                 budget += cost;
             } else {
                 break;
@@ -288,8 +332,119 @@ function translate_hyper_stat_levels_to_stats(hyper_stat_levels){
     return hyper_stats;
 }
 
+function add_buffs(option_list){
+    // First gets buffs and then adds them to each option in the list
+
+    // Get buffs
+    buff_list = get_activated_buffs()
+
+    // Add them to the options
+    var new_option_list = [];
+    $.each(option_list, function(index, option){
+        option["att"]  += buff_list["att"];
+        option["dmg"]  += buff_list["dmg"];
+        option["cdmg"] += buff_list["cdmg"];
+        option["boss"] += buff_list["boss"];
+        option["ied"] = calculate_new_ied(option["ied"], buff_list["ied"]);
+        new_option_list.push(option);
+    })
+
+    return new_option_list;
+}
+
 function calculate_new_ied(old_ied, new_ied){
     return 100 * (1 - (1-old_ied/100) * (1-new_ied/100));
+}
+
+function get_activated_buffs(){
+    // This function gets activated buffs depending on checkmarks
+
+    buff_list = {"dmg":0, "boss":0, "cdmg":0, "ied":0, "att":0};
+
+    // Go through each checkbox ...
+    // Link skills
+    if($('#toggle-switch-cadena').is(':checked')){
+        buff_list["dmg"] += 12;
+    }
+    if($('#toggle-switch-ark').is(':checked')){
+        buff_list["dmg"] += 16;
+    }
+    if($('#toggle-switch-mage').is(':checked')){
+        buff_list["dmg"] += 9;
+        buff_list["ied"]  = calculate_new_ied(buff_list["ied"], 9);
+    }
+    if($('#toggle-switch-thief').is(':checked')){
+        buff_list["dmg"] += 9;
+    }
+    if($('#toggle-switch-illium').is(':checked')){
+        buff_list["dmg"] += 12;
+    }
+    if($('#toggle-switch-kain').is(':checked')){
+        buff_list["dmg"] += 17/2;
+    }
+
+    // Guild skills
+    if($('#toggle-switch-gskill-boss').is(':checked')){
+        buff_list["boss"] += 30;
+    }
+    if($('#toggle-switch-gskill-dmg').is(':checked')){
+        buff_list["dmg"] += 30;
+    }
+    if($('#toggle-switch-gskill-cdmg').is(':checked')){
+        buff_list["cdmg"] += 30;
+    }
+    if($('#toggle-switch-gskill-ied').is(':checked')){
+        buff_list["ied"]  = calculate_new_ied(buff_list["ied"], 30);
+    }
+
+    // Other
+    if($('#toggle-switch-echo').is(':checked')){
+        buff_list["att"] += 4;
+    }
+    if($('#toggle-switch-boss-dmg-pot').is(':checked')){
+        buff_list["boss"] += 20;
+    }
+    if($('#toggle-switch-ied-pot').is(':checked')){
+        buff_list["ied"] = calculate_new_ied(buff_list["ied"], 20);
+    }
+    if($('#toggle-switch-home').is(':checked')){
+        buff_list["boss"] += 15;
+    }
+    if($('#toggle-switch-soul').is(':checked')){
+        buff_list["att"] += 3;
+    }
+
+    return buff_list;
+}
+
+function generate_table(result_options){
+    // get a reference to the existing table element
+    $("#result_table tr").remove(); 
+    const table = document.getElementById("result_table");
+    table.classList.add("bordered-table");
+    
+
+    // create a header row if necessary
+    const headerRow = table.createTHead().insertRow();
+    const wseHeader = headerRow.insertCell();
+    wseHeader.textContent = "WSE";
+    const famHeader = headerRow.insertCell();
+    famHeader.textContent = "Familiars";
+    const hyperHeader = headerRow.insertCell();
+    hyperHeader.textContent = "Hyperstats";
+
+    // create a row for each item in the data array
+    for (let i = 0; i < result_options.length; i++) {
+        const row = table.insertRow();
+        const wseCell = row.insertCell();
+        wseCell.innerHTML = "Weapon: " + JSON.stringify(result_options[i].wse[0]) + "<br>Secondary: " + JSON.stringify(result_options[i].wse[1]) + "<br>Emblem: " + JSON.stringify(result_options[i].wse[2]);
+        //wseCell.textContent = JSON.stringify(result_options[i].wse);
+        const famCell = row.insertCell();
+        famCell.innerHTML = "Familiar 1: " + JSON.stringify(result_options[i].fams[0]) + "<br>Familiar 2: " + JSON.stringify(result_options[i].fams[1]) + "<br>Familiar 3: " + JSON.stringify(result_options[i].fams[2]);
+        //famCell.textContent = JSON.stringify(result_options[i].fams);
+        const hyperCell = row.insertCell();
+        hyperCell.innerHTML = JSON.stringify(result_options[i].hypers);
+    }
 }
 
 $(document).ready(function() {
@@ -301,7 +456,15 @@ $(document).ready(function() {
         object[$("#base_stats_type").find(":selected").val()] = parseFloat($("#base_stats_amount_input").val());
         base_stat_array.push(object);
 
-        $("#base_stats_to_add").append("<p>"+$("#base_stats_type").find(":selected").text()+": "+$("#base_stats_amount_input").val()+"</p>")
+        var htmltoadd = "";
+        htmltoadd += '<div class="card" style="width: 18rem;">';
+        htmltoadd += '<div class="card-body">';
+        htmltoadd += '    <h5 class="card-title">'+$("#base_stats_type").find(":selected").text()+'</h5>';
+        htmltoadd += '    <h6 class="card-subtitle mb-2 text-muted">'+$("#base_stats_amount_input").val()+'</h6>';
+        htmltoadd += '</div>';
+        htmltoadd += '</div>';
+        $("#base_stats_to_add").append(htmltoadd);
+        console.log(base_stat_array);
     });
 
     // Add listeners for adding familiars
@@ -314,7 +477,21 @@ $(document).ready(function() {
         familiars[counter] = object;
         counter++;
 
-        $("#fams_to_add").append("<p>"+$("#fam_stats_type1").find(":selected").text()+": "+$("#fam_amount1_input").val()+", "+$("#fam_stats_type2").find(":selected").text()+": "+$("#fam_amount2_input").val()+"</p>");
+        var htmltoadd = "";
+        htmltoadd += '<div class="card" style="width: 18rem;">';
+        htmltoadd += '<div class="card-body">';
+        htmltoadd += '    <h5 class="card-title">Familiar</h5>';
+
+        if($("#fam_stats_type1").find(":selected").text() != "None"){
+            htmltoadd += '    <h6 class="card-subtitle mb-2 text-muted">'+$("#fam_stats_type1").find(":selected").text()+':\t'+$("#fam_amount1_input").val()+'</h6>';
+        }
+        if($("#fam_stats_type2").find(":selected").text() != "None"){
+            htmltoadd += '    <h6 class="card-subtitle mb-2 text-muted">'+$("#fam_stats_type2").find(":selected").text()+':\t'+$("#fam_amount2_input").val()+'</h6>';
+        }
+
+        htmltoadd += '</div>';
+        htmltoadd += '</div>';
+        $("#fams_to_add").append(htmltoadd);
     });
 
     $("#optimize_btn").click(function() {
@@ -322,17 +499,32 @@ $(document).ready(function() {
         // 1. Create all WSE
         var option_list = null;
         option_list = create_all_wse_options();
+        console.log("Added all WSE options");
 
         // 2. Get each option with each familiar combination
         option_list = add_familiars_options(familiars, option_list);
+        console.log("Added all familiar options");
 
-        // 3. Get each option with each hyper stat combination
-        option_list = add_hyper_stat_options(available_pts, option_list);
-
-        // 4. Add base stats
+        // 3. Add base stats
         option_list = add_base_stats_yourself(base_stat_array, option_list)
+        console.log("Added base stats");
 
-        console.log(option_list);
+        // 4. Add buffs
+        option_list = add_buffs(option_list);
+        console.log("Added buffs");
+
+        // 5. Get each option with each hyper stat combination, but don't save all possibilities since it'll blow up in memory
+        var available_pts = parseFloat($("#hyper_stats_amount_input").val());
+        option_list = add_hyper_stat_options(available_pts, option_list);
+        console.log("Added all hyper stat options");
+
+        // 6. Optimize
+        //sorted_option_list = optimize(option_list);
+        console.log("Sorted");
+
+        
+        generate_table(option_list);
+        //console.log(sorted_option_list.slice(0,100));
     });
 
 })
